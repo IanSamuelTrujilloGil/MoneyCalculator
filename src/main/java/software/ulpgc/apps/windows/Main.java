10e6ef45a07b0dfdfa6ff0271a9fc7420bd9377e
IanSamuelTrujilloGil <iansamueltrujillo@@gmail.com>
2024-12-23 20:22:46 +0000

    Removed IOException from main method signature.

--- src/main/java/software/ulpgc/apps/windows/Main.java
+++ src/main/java/software/ulpgc/apps/windows/Main.java
@@ -4,16 +4,13 @@ import software.ulpgc.architecture.control.CalculateCommand;
 import software.ulpgc.architecture.control.ResetCommand;
 import software.ulpgc.architecture.io.*;
 import software.ulpgc.architecture.model.Currency;
-
-
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 public class Main {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         File file = new File("src/main/resources/currencies.tsv");
         List<Currency> currencyList = new FileCurrencyLoader(file, new TsvCurrencyDeserializer()).load();
         MainFrame mainFrame = new MainFrame(currencyList);
